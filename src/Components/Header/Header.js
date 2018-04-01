bb28ef23a2e2182f7b48e746254dbdd26b5f678e
ansarimofid <ansarimofid@gmail.com>
2018-04-01 18:29:41 +0530

    Adds header in the website

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,20 +1,39 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
+import logo from './statics/img/logo_hapramp_regular.png'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import faGooglePlay from '@fortawesome/fontawesome-free-brands/faGooglePlay'
+import faChrome from '@fortawesome/fontawesome-free-brands/faChrome'
 
-// import './Header.css'
+import './statics/css/Header.css'
 
 const Header = () => (
   <div className="uk-container">
-    <header is uk-sticky="show-on-up: true;animation: uk-animation-slide-top;">
+    <header className="hr-header" is uk-sticky="show-on-up: true;animation: uk-animation-slide-top;">
       <nav className="uk-navbar uk-navbar-container uk-navbar-transparent">
-        <div className="uk-navbar-right uk-nav-center-sm">
+      
+        <div className="uk-navbar-left">
+          <NavLink className="uk-navbar-item uk-logo title-text uk-text-bold text-primary" exact to='/'>
+            <img className= "logo-icon"  src={logo} alt="HapRamp Logo"/> HAPRAMP
+          </NavLink>
+        </div>
+      
+        <div className="uk-navbar-center uk-nav-center-sm">
           <ul className="uk-navbar-nav">
-            <li><NavLink exact to='/' activeClassName="active">Home</NavLink></li>
-            <li><NavLink to='/about' activeClassName="active">About</NavLink></li>
-            <li><NavLink exact to='/faqs' activeClassName="active">FAQs</NavLink></li>
-            {/*<li><NavLink to='/collections' activeClassName="active">Collections</NavLink></li>*/}
+            <li><NavLink className="title-text" exact to='/' activeClassName="active">Home</NavLink></li>
+            <li><NavLink className="title-text" to='/about' activeClassName="active">About</NavLink></li>
+            <li><NavLink className="title-text" exact to='/faqs' activeClassName="active">FAQs</NavLink></li>
           </ul>
         </div>
+
+        <div className="uk-navbar-right uk-nav-center-sm">
+          <span className="uk-text-uppercase uk-margin-small-right">Launching Soon On</span>
+          <a href="" className="uk-margin-small-right uk-text-large" target="_blank"><FontAwesomeIcon icon={faGooglePlay}/></a>
+          <a href="" className="uk-text-large" target="_blank"><FontAwesomeIcon icon={faChrome}/></a>
+          
+
+        </div>
+        
       </nav>
     </header>
   </div>
