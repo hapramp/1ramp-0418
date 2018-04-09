664ba1e2f2431ab12091090216a1ad6dcb21e084
ansarimofid <ansarimofid@gmail.com>
2018-04-09 11:29:58 +0530

    Adds AboutText to homepage

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from './Components/Hero'
+import AboutText from './Components/AboutText'
 
 import './statics/css/About.css'
 
 const About = () => (
   <div>
     <Hero/>
+    <AboutText/>
   </div>
 );
 
