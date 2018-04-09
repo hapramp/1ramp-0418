bc9d9341460f7c8936394ef393d831c2382d9295
ansarimofid <ansarimofid@gmail.com>
2018-04-09 11:31:39 +0530

    Adds AboutText to homepage

---
+++ src/Pages/About/Components/AboutText.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+
+const AboutText = () => (
+  // <!--Hero Starts-->
+  <section className="uk-container qa-view">
+    <div className="uk-padding width-100"></div>
+    <div className="uk-padding-small width-100"></div>
+
+    <div className="uk-grid">
+      <div className="uk-width-1-6@l"></div>
+      <div className="uk-width-2-3@l">
+        <h3 className={'section-title '}>Little About Us</h3>
+        <div className="uk-padding-small width-100"></div>
+        <p>
+          HapRamp is for all the creators,HapRamp is a decentralized social media platform for the creative communities.
+          If you are a learner, hobbyist, professional or just want to browse through the creativity, HapRamp is for you. Join the communities you are interested in and start exploring.
+          On HapRamp creators are the owner of their data.A platform with security and privacy as the core properties.
+        </p>
+        <p>
+          We imagine HapRamp as a place where all the creative communities share their valuable work, earn money, and grow together.
+          For years we had a dream to create a dedicated platforms for creators which caters to their needs and provides exactly what they need.Join HapRamp and we together will create the most happening platform on the Internet.HapRamp platform is built on STEEM blockchain.
+        </p>
+        <p><strong>Team HapRamp.</strong></p>
+      </div>
+    </div>
+    <div className="uk-padding width-100"></div>
+    <div className="uk-padding-small width-100"></div>
+    <div className="uk-padding-small width-100"></div>
+  </section>
+);
+
+export default AboutText
